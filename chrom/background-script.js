96bb2f05f9d7a12ea846e8947a4f71ebb96ea03d
liyawei <yawei.li@leancloud.rocks>
2018-01-30 11:24:06 +0800

    demo

---
+++ chrom/background-script.js
@@ -0,0 +1,23 @@
+chrome.runtime.onConnect.addListener(function(port) {
+  function onResponse(sourceId) {
+    if (!sourceId || !sourceId.length) {
+      port.postMessage('PermissionDeniedError')
+    } else {
+      port.postMessage({ sourceId: sourceId })
+    }
+  }
+
+  function onMessage(msg) {
+    console.log('onMessage', msg)
+    if (msg !== 'requestScreenSourceId') {
+      return
+    }
+    chrome.desktopCapture.chooseDesktopMedia(
+      ['screen', 'window'],
+      port.sender.tab,
+      onResponse
+    )
+  }
+
+  port.onMessage.addListener(onMessage)
+})
